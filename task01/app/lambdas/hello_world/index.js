3343bf69f1340af41d385f0a56e396942fae4b4e
raulhabits <raulhabits@gmail.com>
2024-04-29 14:18:43 -0500

    Update index.js

--- task01/app/lambdas/hello_world/index.js
+++ task01/app/lambdas/hello_world/index.js
@@ -2,7 +2,7 @@ exports.handler = async (event) => {
     // TODO implement
     const response = {
         statusCode: 200,
-        body: JSON.stringify('Hello from Lambda!'),
+        body: 'Hello from Lambda',
     };
     return response;
 };
