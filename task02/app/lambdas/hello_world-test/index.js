a4a0d8a06a018bf4a2fe08e8d8de42229c27191f
raulhabits <raulhabits@gmail.com>
2024-04-30 12:41:01 -0500

    Add files via upload

--- task02/app/lambdas/hello_world-test/index.js
+++ task02/app/lambdas/hello_world-test/index.js
@@ -1,8 +1,35 @@
+/*
 exports.handler = async (event) => {
     // TODO implement
-    const response = {
-        statusCode: 200,
-        message: 'Hello from Lambda',
-    };
+
+    return response;
+};
+*/
+
+const express = require('express');
+const app = express();
+const serverless = require('serverless-http');
+
+const response = {
+    statusCode: 200,
+    message: 'Hello from Lambda',
+};
+
+app.get('/hello', (req, res) => {
+    res.json(response);
+});
+
+const handler = serverless(app);
+
+const startServer = async () => {
+    app.listen(3000, () => {
+      console.log("listening on port 3000!");
+    });
+}
+
+startServer();
+
+module.exports.handler = (event, context, callback) => {
+    const response = handler(event, context, callback);
     return response;
 };
