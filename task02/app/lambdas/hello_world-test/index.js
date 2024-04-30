b3c9904fa0c6c0e119ff6a6a57c25376ec9fffa8
raulhabits <raulhabits@gmail.com>
2024-04-30 09:30:39 -0500

    Add files via upload

---
+++ task02/app/lambdas/hello_world-test/index.js
@@ -0,0 +1,8 @@
+exports.handler = async (event) => {
+    // TODO implement
+    const response = {
+        statusCode: 200,
+        message: 'Hello from Lambda',
+    };
+    return response;
+};
