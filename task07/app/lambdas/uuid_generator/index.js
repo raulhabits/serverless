d97742199a4b14a5f5183b612607426d89ad0191
raulhabits <raulhabits@gmail.com>
2024-05-15 08:05:22 -0500

    Add files via upload

---
+++ task07/app/lambdas/uuid_generator/index.js
@@ -0,0 +1,30 @@
+const { v4: uuidv4 } = require("uuid");
+const AWS = require("aws-sdk");
+const s3 = new AWS.S3();
+
+const targetBucket = process.env.target_bucket;
+
+var roundToNearestMinute = function (date) {
+    var coeff = 1000 * 60 * 1; // <-- Replace {1} with interval
+
+    return new Date(Math.round(date.getTime() / coeff) * coeff);
+};
+
+exports.handler = async (event) => {
+
+    console.log(event);
+    let ids = [...Array(10).keys()].map(index => uuidv4());
+    let content = {ids};
+
+    let key = roundToNearestMinute(new Date(event.time));
+
+	const params = {
+		Bucket: targetBucket,
+		Key: key,
+		Body: JSON.stringify(content, null, 2)
+	};
+    
+    await s3.putObject(params).promise()
+        .then(data => console.log(data))
+        .catch(err => console.log(err, err.stack));
+};
