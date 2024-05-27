88414b9b4fed622ccfa912c37f46f881683318ec
raulhabits <raulhabits@gmail.com>
2024-05-27 01:25:27 -0500

    Update index.js

--- task08/app/lambdas/layers/sdk_layer/index.js
+++ task08/app/lambdas/layers/sdk_layer/index.js
@@ -5,7 +5,7 @@ const instance = axios.create();
 instance.defaults.timeout = 5000;
 
 async function get() {
-    response = await instance.get("https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&hourly=temperature_2m,relative_humidity_2m,wind_speed_10m&format=json&timeformat=unixtime");
+    response = await instance.get("https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&current=temperature_2m,relative_humidity_2m,apparent_temperature,is_day,precipitation,rain,showers,snowfall,weather_code,cloud_cover,pressure_msl,surface_pressure,wind_speed_10m,wind_direction_10m,wind_gusts_10m&hourly=temperature_2m,relative_humidity_2m,wind_speed_10m&format=json&timeformat=unixtime");
     let data = {...response.data};
     let time = data.hourly.time.map(elem => new Date(elem * 1000).toISOString().substring(0,16))
     data.hourly = time;
