235b4a5cff43a9f1493c408f000d9b7bcb6d1acb
raulhabits <raulhabits@gmail.com>
2024-05-27 01:01:37 -0500

    Update index.js

--- task08/app/lambdas/layers/sdk_layer/index.js
+++ task08/app/lambdas/layers/sdk_layer/index.js
@@ -7,7 +7,7 @@ instance.defaults.timeout = 5000;
 async function get() {
     response = await instance.get("https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&hourly=temperature_2m&format=json&timeformat=unixtime");
     let data = {...response.data};
-    let time = data.hourly.time.map(elem => new Date(elem * 1000).toISOString())
+    let time = data.hourly.time.map(elem => new Date(elem * 1000).toISOString().toString())
     data.hourly = time;
     return response.data;
 }
