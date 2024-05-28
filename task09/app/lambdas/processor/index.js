f3d3d4483ebd247b08cd645fe8722fc8a3d49fb4
raulhabits <raulhabits@gmail.com>
2024-05-27 20:35:29 -0500

    Update index.js

--- task09/app/lambdas/processor/index.js
+++ task09/app/lambdas/processor/index.js
@@ -10,7 +10,7 @@ const docClient = new AWS.DynamoDB.DocumentClient();
 const tableName = process.env.target_table;
 
 async function get() {
-    response = await instance.get("https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&hourly=temperature_2m&format=json&timeformat=unixtime");
+    response = await instance.get("https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&current=temperature_2m,wind_speed_10m&hourly=temperature_2m,relative_humidity_2m,wind_speed_10m");
     return response.data;
 }
 
