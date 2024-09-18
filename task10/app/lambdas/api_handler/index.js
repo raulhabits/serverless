f022785463f29450a7ac7919eb5f34449ea41e1b
raulhabits <raulhabits@gmail.com>
2024-09-18 01:33:07 -0500

    Update index.js

--- task10/app/lambdas/api_handler/index.js
+++ task10/app/lambdas/api_handler/index.js
@@ -182,14 +182,13 @@ app.post('/reservations', async (req, res) => {
         TableName: tablesTableDynamodb,
         KeyConditionExpression: `number = :value`,
         ExpressionAttributeValues: {
-            ':value': { N: body.tableNumber },
-            
+            ':value': body.tableNumber
         },
     };
 
     console.log("AddReservation.tablesTableDynamodb.params", params);
     try {
-        let queryResult = await docClient.query(params).promise();
+        let queryResult = await docClient.scan(params).promise();
 
         console.log("SUCCESSFULL GET", queryResult);
 
